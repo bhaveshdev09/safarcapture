e6873d899cf5fb97ca14f153630d1ad21dd381a3
Bhavesh Patil <patilbhavesh991209@gmail.com>
2024-01-07 22:10:53 +0530

    start: setup project, remove old setup and static file configuration

--- myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from myapp import views
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("about/", views.about, name="about"),
-    path("blogs/", views.about, name="blogs"),
-    path("contact/", views.about, name="contact"),
-    path("faq/", views.faq, name="faq"),
-]
